bb63580c7808fc27f995e033c7a3c57383925acb
huykt1111 <huylmht10@gmail.com>
2022-11-24 09:05:22 +0700

    Login & Logout & UserInfo

---
+++ server.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const cookieParser = require('cookie-parser')
+
+
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+app.use(fileUpload({
+    useTempFile: true
+}))
+
+// Routers
+app.use('/user', require('./routers/userRouter'))
+
+// Connect to mongoose
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI, {
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if(err) throw err;
+    console.log('Connected to MongoDB')
+})
+
+app.get('/', (req, res) => {
+    res.json({msg: "Welcome my channel , please subcribe for us. Thanks"})
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT)
+})
