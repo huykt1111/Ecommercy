b09c184bf479c8f60edd0b44e203c93d45a8f078
huykt1111 <huylmht10@gmail.com>
2022-11-27 20:14:55 +0700

    [FrontEnd] Product - DetailProduct

--- Fontend/src/components/mainpages/Pages.js
+++ Fontend/src/components/mainpages/Pages.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Products from './products/Products'
+import DetailProduct from './detailProduct/DetailProduct'
 import Cart from './cart/Cart'
 import Login from './auth/Login'
 import Register from './auth/Register'
@@ -10,6 +11,7 @@ function Pages() {
   return (
     <Routes>
       <Route path='/' exact element={<Products />} />
+      <Route path='/detail/:id' exact element={<DetailProduct />} />
       <Route path='/login' exact element={<Login />} />
       <Route path='/register' exact element={<Register />} />
       <Route path='/cart' exact element={<Cart />} />
--- Fontend/src/components/mainpages/products/Products.js
+++ Fontend/src/components/mainpages/products/Products.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {GlobalState} from '../../../GlobalState'
+import ProductItem from '../utils/productitem/ProductItem'
 
 function Products() {
+  const state = useContext(GlobalState)
+  const [products] = state.productsAPI.products
   return (
-    <div>Products</div>
+    <div className="products">
+      {
+        products.map(product =>  {
+          return <ProductItem key={product._id} product={product}/>
+        })
+      }
+    </div>
   )
 }
 
