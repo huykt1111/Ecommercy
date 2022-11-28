0eaf663064f9bf105c757dd41fd7000e84457286
huykt1111 <huylmht10@gmail.com>
2022-11-28 16:46:36 +0700

    [Frontend] Cart

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -95,6 +95,20 @@ const userController = {
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
+    },
+    addCart: async (req, res) => {
+        try {
+            const user = await Users.findById(req.user.id)
+            if(!user) return res.status(400).json({msg: "User does not exits."})
+
+            await Users.findOneAndUpdate({_id: req.user.id}, {
+                cart: req.body.cart
+            })
+
+            return res.json({msg: "Added to cart"})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
     }
 }
 
--- Fontend/src/components/headers/Header.js
+++ Fontend/src/components/headers/Header.js
@@ -4,9 +4,38 @@ import Menu from './icon/menu.svg'
 import Close from './icon/close.svg'
 import Cart from './icon/cart.svg'
 import {Link} from 'react-router-dom'
+import axios from 'axios'
 
 function Header() {
-  const value = useContext(GlobalState)
+  const state = useContext(GlobalState)
+  const [isLogged] = state.userAPI.isLogged 
+  const [isAdmin] = state.userAPI.isAdmin
+  const [cart] = state.userAPI.cart
+
+  const logoutUser = async () => {
+    await axios.get('/user/logout')
+    localStorage.clear()
+    window.location.href = '/'
+  }
+  
+  const adminRouter = () => {
+    return (
+        <>
+            <li><Link to="/create_product">CREATE PRODUCT</Link></li>
+            <li><Link to="/category">CATEGORIES</Link></li>
+        </>
+    )
+  }
+
+  const loggedRouter = () => {
+    return (
+        <>
+            <li><Link to="/history">HISTORY</Link></li>
+            <li><Link to="/" onClick={logoutUser}>LOGOUT</Link></li>
+        </>
+    )
+  }
+
   return (
     <div>
         <header>
@@ -16,24 +45,32 @@ function Header() {
 
             <div className="logo">
                 <h1>
-                    <Link to="/">DevAt Shop</Link>
+                    <Link to="/">{isAdmin ? 'ADMIN' : 'DevaT Shop'}</Link>
                 </h1>
             </div>
 
             <ul>
-                <li><Link to="/">Products</Link></li>
-                <li><Link to="/login">Login ♦ Register</Link></li>
+                <li><Link to="/">{isAdmin ? 'PRODUCTS' : 'SHOP'}</Link></li>
+
+                {isAdmin && adminRouter()}
+                {
+                    isLogged ? loggedRouter() : <li><Link to="/login">Login ♦ Register</Link></li>
+                }
+
                 <li>
                     <img src={Close} alt="" width="30" className="menu" />
                 </li>
             </ul>
 
-            <div className="cart-icon" >
-                <span>0</span>
-                <Link to="/cart">
-                    <img src={Cart} alt="" width="30" />
-                </Link>
-            </div>
+            {
+                isAdmin ? ''  
+                :<div className="cart-icon" >
+                    <span>{cart.length}</span>
+                    <Link to="/cart">
+                        <img src={Cart} alt="" width="30" />
+                    </Link>
+                </div>
+            }
         </header>
     </div>
   )
